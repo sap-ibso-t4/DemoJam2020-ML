8e56ee1af9505f4a8c0f6c403f43f5a8085b53ea
zzfancitizen <zzfancitizen@gmail.com>
2020-10-10 17:18:26 +0800

    Normalize CSV

--- tool/csvNormalization.py
+++ tool/csvNormalization.py
@@ -1,9 +1,18 @@
 import pandas as pd
+import numpy as np
 
 
-def normalize_csv(PATH):
-    pass
+def normalize_csv(path):
+    df = pd.read_csv(path)
+    df_pre = df.select_dtypes(include=['float64', 'int64'])
+    df_pre = df_pre.drop(columns=['material ID', 'category', 'hierarchy category', 'level'])
+    df_norm = df_pre.apply(
+        lambda x: (x - np.min(x)) / (np.max(x) - np.min(x)) if (np.max(x) - np.min(x)) != 0 else 0)
+    df_head = list(df_norm.columns)
+    df[df_head] = df_norm
+    df.to_csv(path_or_buf='../data/raw/pistonsNormal.csv', sep=",", index=False)
 
 
 if __name__ == "__main__":
-    pass
+    PATH = '../data/raw/pistons.csv'
+    normalize_csv(PATH)
