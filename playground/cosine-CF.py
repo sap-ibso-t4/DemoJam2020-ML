8fa6b0da5cebed804855ef37256d884c7a88bd14
zzfancitizen <zzfancitizen@gmail.com>
2020-10-10 09:21:17 +0800

    Fix csv

--- playground/cosine-CF.py
+++ playground/cosine-CF.py
@@ -1,25 +1,25 @@
 import numpy as np
 
 
-def Cosine(dataA, dataB):
-    arrayA = np.array(dataA)
-    arrayB = np.array(dataB)
-    sumData = arrayA.T.dot(arrayB)
-    print(sumData)
-    denom = np.linalg.norm(arrayA) * np.linalg.norm(arrayB)
-    return 0.5 + 0.5 * (sumData / denom)
+def cosine(data_a, data_b):
+    array_a = np.array(data_a)
+    array_b = np.array(data_b)
+    sum_data = array_a.T.dot(array_b)
+    print(sum_data)
+    denom = np.linalg.norm(array_a) * np.linalg.norm(array_b)
+    return 0.5 + 0.5 * (sum_data / denom)
 
 
-def CosineBeta(dataA, dataB):
-    arrayA = np.mat(dataA)
-    arrayB = np.mat(dataB)
-    sumData = arrayA * arrayB.T  # 若列为向量则为 dataA.T * dataB
-    denom = np.linalg.norm(arrayA) * np.linalg.norm(arrayB)
-    return 0.5 + 0.5 * (sumData / denom)[0, 0]
+def cosine_beta(data_a, data_b):
+    array_a = np.mat(data_a)
+    array_b = np.mat(data_b)
+    sum_data = array_a * array_b.T  # 若列为向量则为 data_a.T * data_b
+    denom = np.linalg.norm(array_a) * np.linalg.norm(array_b)
+    return 0.5 + 0.5 * (sum_data / denom)[0, 0]
 
 
 if __name__ == "__main__":
-    User_A = [1, 0, 1, 1]
-    User_B = [1, 1, 0, 1]
-    print('User_A与User_B的余弦相似度为: ', Cosine(User_A, User_B))
-    print('User_A与User_B的余弦相似度为: ', CosineBeta(User_A, User_B))
+    User_A = [0.01, 0, 0.01, 0.01]
+    User_B = [100, 100, 0, 100]
+    print('User_A与User_B的余弦相似度为: ', cosine(User_A, User_B))
+    print('User_A与User_B的余弦相似度为: ', cosine_beta(User_A, User_B))
---
+++ tool/csvNormalization.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+
+def normalize_csv(PATH):
+    pass
+
+
+if __name__ == "__main__":
+    pass
